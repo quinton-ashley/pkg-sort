7689c2fb0ccc6ff934ae34ebcfd316cb155b53a7
Quinton Ashley <qashto@gmail.com>
2018-06-01 22:17:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# pkg-sort
+
+Alphabetize your package.json
+
+## Installation
+
+```javascript
+npm install -g pkg-sort
+```
+
+## Usage
+
+Run this from a directory with a `package.json` file:
+
+```javascript
+pkg-sort
+```
+
+## Credit
+
+<https://github.com/davidguttman/cdo-package>
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = function(pkg) {
+  pkg = sortObj(pkg);
+
+  if (pkg.dependencies) {
+    pkg.dependencies = sortObj(pkg.dependencies);
+  }
+
+  if (pkg.devDependencies) {
+    pkg.devDependencies = sortObj(pkg.devDependencies);
+  }
+
+  return pkg;
+}
+
+function sortObj(obj) {
+  var sorted = {};
+  Object.keys(obj).sort().forEach(function(key) {
+    sorted[key] = obj[key];
+  })
+  return sorted;
+}
